35805d2d55d438dcc411e2a5f1a2791df4331fb9
rianto <rianto.rnt@gmail.com>
2023-02-04 17:06:57 +0800

    + POST Request to data json

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 
 const app = express()
 
-const port = 3000
+app.use(express.json()) // to support JSON-encoded bodies
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/data/json/simple-tours.json`)
@@ -14,6 +14,28 @@ app.get('/api/v1/tours', (req, res) => {
     .status(200)
     .json({ status: 'success', result: tours.length, data: { tours } })
 })
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1
+  const newTour = Object.assign({ id: newId }, req.body)
+
+  tours.push(newTour)
+
+  fs.writeFile(
+    `${__dirname}/data/json/simple-tours.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      })
+    }
+  )
+})
+
+const port = 3000
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)

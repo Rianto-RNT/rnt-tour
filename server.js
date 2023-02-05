a2835ed180ef605b92671cb0bd731586e4f7a913
rianto <rianto.rnt@gmail.com>
2023-02-05 13:36:22 +0800

    + PATCH request

--- server.js
+++ server.js
@@ -12,22 +12,22 @@ const tours = JSON.parse(
 app.get('/api/v1/tours', (req, res) => {
   res
     .status(200)
-    .json({ status: 'success', result: tours.length, data: { tours } })
+    .json({ status: 'Success Get All', result: tours.length, data: { tours } })
 })
 
 app.get('/api/v1/tours/:id', (req, res) => {
   console.log(req.params)
-  const id = req.params.id * 1
+  const id = Number(req.params.id)
   const tour = tours.find((el) => el.id === id)
 
   if (id > tours.length) {
     return res.status(404).json({
-      status: 'Not found',
-      message: 'ID yang anda cari tidak ditemukan',
+      status: 'Not Found',
+      message: `ID:'${Number(req.params.id)}' yang anda cari tidak ditemukan!`,
     })
   }
 
-  res.status(200).json({ status: 'success', data: { tour } })
+  res.status(200).json({ status: 'Success Get One', data: { tour } })
 })
 
 app.post('/api/v1/tours', (req, res) => {
@@ -45,11 +45,47 @@ app.post('/api/v1/tours', (req, res) => {
         return
       }
 
-      // Return a success response
       res.status(201).json({
-        status: 'success',
+        status: 'Success Create',
         data: {
           tour: newTour,
+        },
+      })
+    }
+  )
+})
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const tourId = Number(req.params.id)
+  const tourIndex = tours.findIndex((tour) => tour.id === tourId)
+
+  if (tourIndex === -1) {
+    return res.status(404).json({
+      status: 'Failed to update',
+      message: `ID:'${Number(
+        req.params.id
+      )}' yang anda update tidak ditemukan!`,
+    })
+  }
+
+  // Update the tour properties
+  const updatedTour = Object.assign({}, tours[tourIndex], req.body)
+  tours[tourIndex] = updatedTour
+
+  fs.writeFile(
+    `${__dirname}/data/json/simple-tours.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        res.status(500).send('Error writing to file')
+        return
+      }
+
+      // Return a success response
+      res.status(200).json({
+        status: 'Success Update',
+        data: {
+          tour: updatedTour,
         },
       })
     }
